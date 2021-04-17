1f8a2332fae7c0417cf567c6516c866d1a88049d
Mariana <m.zamorab@uniandes.edu.co>
2021-04-16 23:31:16 -0500

    Taller 1 Typescript

---
+++ tallerTypescript/scripts/dataCourses.js
@@ -0,0 +1,8 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW", "César Forero", 3),
+    new Course("Infraestructura Tecnológica", "Jesse Padilla", 3),
+    new Course("Psicología Social", "Juan Pablo Aranguren", 3),
+    new Course("MEL", "Nelson Sanchez", 3),
+    new Course("Sistemas Transaccionales", "Claudia", 3)
+];
